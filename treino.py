1b66b38effafaae2a7a0029e8c57b117f87cd1eb
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-26 21:33:31 -0300

    Update treino.py

--- treino.py
+++ treino.py
@@ -1,1 +1,21 @@
+from abc import ABC, abstractmethod
 
+# ==========================
+# Classe Abstrata Treino
+# ==========================
+
+class Treino(ABC):
+
+    NIVEIS = ("iniciante", "intermediario", "avancado")
+
+    def __init__(self, nivel):
+        nivel = nivel.lower()
+
+        if nivel not in self.NIVEIS:
+            raise ValueError("Nível inválido. Use: iniciante, intermediario ou avancado.")
+
+        self.nivel = nivel
+
+    @abstractmethod
+    def calcular_calorias(self):
+        pass
