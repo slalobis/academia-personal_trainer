1b34a36dfc5c7781845be7ec9e69364b05aa79e7
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-26 21:37:43 -0300

    Update treino_forca.py

--- treino_forca.py
+++ treino_forca.py
@@ -1,1 +1,12 @@
+from modelos.treino import Treino
 
+class TreinoForca(Treino):
+
+    def __init__(self, peso_levantado, series, nivel):
+        super().__init__(nivel)
+
+        self.peso_levantado = peso_levantado
+        self.series = series
+
+    def calcular_calorias(self):
+        return self.peso_levantado * self.series * 0.15
