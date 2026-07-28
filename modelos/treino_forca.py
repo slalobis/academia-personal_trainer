20a721e4f7914fc4abe86d241fe8add9dffa0dec
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-28 12:36:23 -0300

    Update treino_forca.py

--- modelos/treino_forca.py
+++ modelos/treino_forca.py
@@ -1,26 +1,49 @@
-from modelos.treino import Treino
-from modelos.dificuldade import Dificuldade
+from treino import Treino
+from dificuldade import Dificuldade
 
-class TreinoForca(Treino, Dificuldade):
 
-    def __init__(self, peso_levantado, series, nivel):
+class TreinoForca(Treino):
+
+    def __init__(self,
+                 exercicio,
+                 peso,
+                 series,
+                 repeticoes,
+                 nivel,
+                 dificuldade):
+
         super().__init__(nivel)
 
-        self.__peso_levantado = peso_levantado
+        self.__exercicio = exercicio
+        self.__peso = peso
         self.__series = series
+        self.__repeticoes = repeticoes
+        self.__dificuldade = dificuldade
 
     @property
-    def peso_levantado(self):
-        return self.__peso_levantado
+    def exercicio(self):
+        return self.__exercicio
+
+    @property
+    def peso(self):
+        return self.__peso
 
     @property
     def series(self):
         return self.__series
 
-    def calcular_calorias(self):
-        return self.__peso_levantado * self.__series * 0.15
+    @property
+    def repeticoes(self):
+        return self.__repeticoes
+
+    @property
+    def dificuldade(self):
+        return self.__dificuldade
 
     def descricao(self):
-        return (f"Treino de Força\n"
-            f"Séries: {self.series}\n"
-            f"Peso: {self.peso} kg")
+
+        return (
+            f"{self.exercicio}\n"
+            f"Nível: {self.nivel}\n"
+            f"Dificuldade: {self.dificuldade.value}"
+        )
