1e2032decde8b75e8a5de43c35ff40631415a2fb
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-28 00:00:29 -0300

    Add Dificuldade class with difficulty calculation method

--- modelos/dificuldade.py
+++ modelos/dificuldade.py
@@ -1,1 +1,11 @@
+class Dificuldade:
 
+    def calcular_dificuldade(self):
+
+        dificuldades = {
+            "iniciante": "Fácil",
+            "intermediario": "Médio",
+            "avancado": "Difícil"
+        }
+
+        return dificuldades[self.nivel]
