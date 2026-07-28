1bb606b3960a76e1be2e6ad3a4714b3b008b7a26
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-28 12:34:42 -0300

    Update aluno.py

--- modelos/aluno.py
+++ modelos/aluno.py
@@ -1,8 +1,17 @@
 class Aluno:
 
-    def __init__(self, nome, idade):
+    def __init__(self,
+                 nome,
+                 idade,
+                 peso,
+                 altura,
+                 objetivo):
+
         self.__nome = nome
         self.__idade = idade
+        self.__peso = peso
+        self.__altura = altura
+        self.__objetivo = objetivo
 
     @property
     def nome(self):
@@ -11,3 +20,15 @@ class Aluno:
     @property
     def idade(self):
         return self.__idade
+
+    @property
+    def peso(self):
+        return self.__peso
+
+    @property
+    def altura(self):
+        return self.__altura
+
+    @property
+    def objetivo(self):
+        return self.__objetivo
