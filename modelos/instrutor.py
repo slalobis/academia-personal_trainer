cd94e166ebd086106edc4f0d21ca8de0df302011
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-28 15:04:35 -0300

    Refactor Instrutor class and add string representation
    Refactor constructor for better readability and add __str__ method for string representation.

--- modelos/instrutor.py
+++ modelos/instrutor.py
@@ -1,6 +1,8 @@
 class Instrutor:
 
-    def __init__(self, nome, cref):
+    def __init__(self,
+                 nome,
+                 cref):
 
         self.__nome = nome
         self.__cref = cref
@@ -12,3 +14,10 @@ class Instrutor:
     @property
     def cref(self):
         return self.__cref
+
+    def __str__(self):
+
+        return (
+            f"Instrutor: {self.nome}\n"
+            f"CREF: {self.cref}"
+        )
