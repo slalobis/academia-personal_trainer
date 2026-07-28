33dd9c3d18e50e664602c11fc60a94976a99d3d7
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-28 15:04:09 -0300

    Update aluno.py

--- modelos/aluno.py
+++ modelos/aluno.py
@@ -5,13 +5,15 @@ class Aluno:
                  idade,
                  peso,
                  altura,
-                 objetivo):
+                 objetivo,
+                 meses_matricula):
 
         self.__nome = nome
         self.__idade = idade
         self.__peso = peso
         self.__altura = altura
         self.__objetivo = objetivo
+        self.__meses_matricula = meses_matricula
 
     @property
     def nome(self):
@@ -32,3 +34,18 @@ class Aluno:
     @property
     def objetivo(self):
         return self.__objetivo
+
+    @property
+    def meses_matricula(self):
+        return self.__meses_matricula
+
+    def __str__(self):
+
+        return (
+            f"Aluno: {self.nome}\n"
+            f"Idade: {self.idade}\n"
+            f"Peso: {self.peso} kg\n"
+            f"Altura: {self.altura} m\n"
+            f"Objetivo: {self.objetivo}\n"
+            f"Tempo de matrícula: {self.meses_matricula} meses"
+        )
