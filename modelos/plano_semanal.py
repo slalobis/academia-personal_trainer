f49657650222deccbac25cfc16b90b1ed1bda59d
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-28 12:37:23 -0300

    Update plano_semanal.py

--- modelos/plano_semanal.py
+++ modelos/plano_semanal.py
@@ -1,27 +1,22 @@
 class PlanoSemanal:
 
-    LIMITE_TREINOS = 5
-
     def __init__(self):
-        self.__treinos = []
+
+        self.__dias = {
+            "Segunda": [],
+            "Terça": [],
+            "Quarta": [],
+            "Quinta": [],
+            "Sexta": []
+        }
 
     @property
-    def treinos(self):
-        return self.__treinos
+    def dias(self):
+        return self.__dias
 
-    def adicionar_treino(self, treino):
+    def adicionar_exercicio(self, dia, treino):
 
-        if len(self.treinos) >= self.LIMITE_TREINOS:
-            return False
-
-        self.treinos.append(treino)
-        return True
-
-    def calcular_calorias_semana(self):
-
-        total = 0
-
-        for treino in self.treinos:
-            total += treino.calcular_calorias()
-
-        return total
+        if dia in self.__dias:
+            self.__dias[dia].append(treino)
+        else:
+            print("Dia inválido.")
