5a47f6ac5f80abc1a879d272a81b7c4f0bac8d2f
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-27 23:53:37 -0300

    Update plano_semanal.py

--- modelos/plano_semanal.py
+++ modelos/plano_semanal.py
@@ -3,7 +3,11 @@ class PlanoSemanal:
     LIMITE_TREINOS = 5
 
     def __init__(self):
-        self.treinos = []
+        self.__treinos = []
+
+    @property
+    def treinos(self):
+        return self.__treinos
 
     def adicionar_treino(self, treino):
 
