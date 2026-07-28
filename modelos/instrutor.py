678e0765f2d90845f828195a4ab6b465aef72334
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-28 12:35:57 -0300

    Update instrutor.py

--- modelos/instrutor.py
+++ modelos/instrutor.py
@@ -1,1 +1,14 @@
+class Instrutor:
 
+    def __init__(self, nome, cref):
+
+        self.__nome = nome
+        self.__cref = cref
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def cref(self):
+        return self.__cref
