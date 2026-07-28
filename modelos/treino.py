b999c2417c46b0c1f47e5d73b84f1b922bb3045f
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-28 15:04:55 -0300

    Update treino.py

--- modelos/treino.py
+++ modelos/treino.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 class Treino(ABC):
 
     def __init__(self, nivel):
+
         self.__nivel = nivel
 
     @property
@@ -12,3 +13,11 @@ class Treino(ABC):
     @abstractmethod
     def descricao(self):
         pass
+
+    @abstractmethod
+    def calcular_calorias(self):
+        pass
+
+    def __str__(self):
+
+        return self.descricao()
