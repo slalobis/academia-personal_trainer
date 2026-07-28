94a9581fb592268a00b2a9412ce8299b45db8028
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-27 23:31:03 -0300

    Update treino.py

--- modelos/treino.py
+++ modelos/treino.py
@@ -9,12 +9,17 @@ class Treino(ABC):
     NIVEIS = ("iniciante", "intermediario", "avancado")
 
     def __init__(self, nivel):
+
         nivel = nivel.lower()
 
         if nivel not in self.NIVEIS:
-            raise ValueError("Nível inválido. Use: iniciante, intermediario ou avancado.")
+            raise ValueError("Nível inválido.")
 
-        self.nivel = nivel
+        self.__nivel = nivel   # atributo privado
+
+    @property
+    def nivel(self):
+        return self.__nivel
 
     @abstractmethod
     def calcular_calorias(self):
