d42a442493e7b250afd8de2986bd04efa40a8c44
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-28 15:43:58 -0300

    Create avaliavel.py

---
+++ modelos/avaliavel.py
@@ -0,0 +1,13 @@
+class Avaliavel:
+
+    def nivel_dificuldade(self):
+        return "Intermediário"
+
+    def recomendacao(self):
+        return "Siga as orientações do personal trainer."class Avaliavel:
+
+    def nivel_dificuldade(self):
+        return "Intermediário"
+
+    def recomendacao(self):
+        return "Siga as orientações do personal trainer."
