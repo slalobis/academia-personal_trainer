de5566b775dbda29df0607c7e98f8dbadbd1d56c
Jotape-Cloud <joao.pedro.rangel@escola.pr.gov.br>
2026-07-29 17:50:16 -0300

    Create __init__.py

---
+++ modelos/__init__.py
@@ -0,0 +1,15 @@
+from .aluno import Aluno
+from .plano_semanal import PlanoSemanal
+from .treino import Treino
+from .treino_forca import TreinoForca
+from .treino_cardio import TreinoCardio
+from .treino_flexibilidade import TreinoFlexibilidade
+
+__all__ = [
+    "Aluno",
+    "PlanoSemanal",
+    "Treino",
+    "TreinoForca",
+    "TreinoCardio",
+    "TreinoFlexibilidade",
+]
